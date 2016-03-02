2c877060df2615b05f7b24aa8329db5e6dcf94a4
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-02 09:05:07 -0300

    Added REST API endpoints, services and repositories

--- Backend/src/main/java/com/unep/wcmc/foodnutrition/model/RecipeIngredient.java
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/model/RecipeIngredient.java
@@ -1,6 +1,17 @@
 package com.unep.wcmc.foodnutrition.model;
 
-public class RecipeIngredient {
+import com.unep.wcmc.foodnutrition.support.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class RecipeIngredient implements BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     private String name;
 
@@ -9,6 +20,16 @@ public class RecipeIngredient {
     private Integer quantityHousehold;
 
     private String householdMeasure;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getName() {
         return name;
