62252e5ff2828f4d3a065066eb1424ca8466c05d
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-02 08:32:29 -0300

    Added REST API endpoints, services and repositories

--- Backend/src/main/java/com/unep/wcmc/foodnutrition/repository/PreservationMethodRepository.java
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/repository/PreservationMethodRepository.java
@@ -4,6 +4,6 @@ import com.unep.wcmc.foodnutrition.model.PreservationMethod;
 import com.unep.wcmc.foodnutrition.support.AbstractRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource
+@RepositoryRestResource(path = "/preservation-methods")
 public interface PreservationMethodRepository extends AbstractRepository<PreservationMethod> {
 }
