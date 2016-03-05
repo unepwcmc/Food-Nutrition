04cb92be940328fa82aaa318edbb62dbcefcd79e
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-05 10:08:33 -0300

    Changes at DEV database

---
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/model/Alcohol.java
@@ -0,0 +1,10 @@
+package com.unep.wcmc.foodnutrition.model;
+
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue(Nutrient.ALCOHOL)
+public class Alcohol extends Nutrient {
+    // Empty
+}
