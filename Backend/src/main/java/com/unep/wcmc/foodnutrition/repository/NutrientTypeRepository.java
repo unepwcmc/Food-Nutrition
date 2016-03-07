6928d91fc19cefcad208d71ea7f048cad48fb225
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-07 18:45:17 -0300

    Added Nutrient type parent endpoint

---
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/repository/NutrientTypeRepository.java
@@ -0,0 +1,17 @@
+package com.unep.wcmc.foodnutrition.repository;
+
+import com.unep.wcmc.foodnutrition.model.NutrientType;
+import com.unep.wcmc.foodnutrition.support.AbstractRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+@RepositoryRestResource(path = "/nutrient-types")
+public interface NutrientTypeRepository extends AbstractRepository<NutrientType> {
+
+    @RestResource(path = "nutrients")
+    List<NutrientType> findByParentName(@Param("parent") String parent);
+
+}
