0c429feeecca15fe68d28f98403748946e64658a
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-08 09:19:04 -0300

    Changes at model

--- Backend/src/main/java/com/unep/wcmc/foodnutrition/model/Food.java
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/model/Food.java
@@ -14,6 +14,8 @@ public class Food implements BaseEntity {
 
     private String name;
 
+    private String description;
+
     @ManyToOne
     @JoinColumn(name = "food_type_id")
     private FoodType foodType;
@@ -117,6 +119,14 @@ public class Food implements BaseEntity {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public String getScientificName() {
