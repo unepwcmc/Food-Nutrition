1e99a128801eb9adbb80a0dfcf0ae3def147b386
integritas-jozecarlos <i4525001314s>
2016-03-07 23:31:39 -0300

    Carbohydrate using the new endpoint

---
+++ Frontend/app/scripts/core/factory/nutrientFactory.js
@@ -0,0 +1,79 @@
+/**
+ * Nutrient Factory Model
+ * @author Jose Carlos
+ * @email jozecarlos.it@gmail.com
+ *
+ */
+define(['angularAMD'], function (angularAMD) {
+
+    'use strict';
+
+    angularAMD.factory('Nutrient', ['$http', '$rootScope', '$log', function ($http, $rootScope, $log) {
+
+        /**
+         *
+         * @param data
+         * @constructor
+         */
+        function Nutrient(data) {
+
+            if (data) {
+                this.setData(data);
+            }
+        }
+
+        /**
+         *
+         * @type {{setData: Function, load: Function, update: Function, getHost: Function}}
+         */
+        Nutrient.prototype = {
+
+            setData: function (data) {
+                angular.extend(this, data);
+            },
+            get: function( id){
+
+                var self = this;
+
+                $http.get( $rootScope.getHost() + "foods/" + id )
+
+                    .success(function (data) {
+                        if (data.message == 'no matches found') {
+                            $rootScope.$broadcast("FOOD_LOAD_ERROR");
+                        } else {
+                            self.setData(data);
+                            $rootScope.$broadcast("FOOD_LOADED");
+                        }
+                    })
+                    .error(function (message) {
+                        $log.error(message);
+                        $rootScope.$broadcast("FOOD_LOAD_ERROR");
+                    });
+            },
+            filterByType: function( type, callback ){
+
+                var self = this;
+
+                $http.get( $rootScope.getHost() + "nutrient-types/search/nutrients?parent=" + type )
+
+                    .success(function ( data, status, headers, config) {
+                        if (data.message == 'no matches found') {
+                            $rootScope.$broadcast("NUTRIENT_TYPE_LOAD_ERROR");
+                        } else {
+                            //self.setData(data);
+                            if(callback)
+                                callback( data, status, headers, config);
+                            $rootScope.$broadcast("NUTRIENT_TYPE_LOADED");
+                        }
+                    })
+                    .error(function (data, status, headers, config) {
+                        $log.error( data.message );
+                        $rootScope.$broadcast("NUTRIENT_TYPE_LOAD_ERROR");
+                    });
+            }
+        };
+
+        return Nutrient;
+
+    }]);
+});
--- Frontend/app/scripts/food/directives/energy.macronutrients.directive.js
+++ Frontend/app/scripts/food/directives/energy.macronutrients.directive.js
@@ -1,23 +1,49 @@
-define(['angularAMD','food/directives/energy.macronutrients.item.directive'], function (angularAMD) {
+define(['angularAMD',
+        'core/factory/nutrientFactory',
+        'food/directives/energy.macronutrients.item.directive'], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('energyMacronutrients', ['$timeout', '$rootScope', 'toastr',
-        function ( $timeout, $rootScope, toastr ) {
+    angularAMD.directive('energyMacronutrients', ['$rootScope','Nutrient', function ( $rootScope, Nutrient ) {
 
             return {
 
                 restrict: 'EA',
                 templateUrl: 'views/food/energy.macronutrients.tab.tpl.html',
 
-                controller: ['$scope', '$http', '$rootScope', '$state', '$q',
-                    function( $scope, $http, $rootScope, $state, $q ) {
+                controller: ['$scope','$rootScope',function( $scope, $rootScope ) {
 
+                         $scope.carbohydrates_polyols= [];
+                         $scope.carbohydrates_fibre = [];
+                         $scope.carbohydrates_poly = [];
+                         $scope.carbohydrates_polyols= [];
+                         $scope.carbohydrates = [];
+
+                         var nutrient = new Nutrient();
+
+                        nutrient.filterByType('FIBRE', function( data, status){
+                            $scope.carbohydrates_fibre = data;
+                        });
+
+                        nutrient.filterByType('SUGARS', function( data, status){
+                            $scope.carbohydrates_sugars = data;
+                        });
+
+
+                        nutrient.filterByType('POLYSACCHARIDES', function( data, status){
+                            $scope.carbohydrates_poly = data;
+                        });
+
+                        nutrient.filterByType('POLYOLS', function( data, status){
+                            $scope.carbohydrates_polyols= data;
+                        });
+
+                        nutrient.filterByType('CARBOHYDRATES_TOTAL', function( data, status){
+                            $scope.carbohydrates = data;
+                        });
 
                     }],
                 link: function (scope, element, attrs) {
-
-
                 }
             };
         }]);
