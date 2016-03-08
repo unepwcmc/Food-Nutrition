2e9f4afbbc8968f8fa32df3b31efa37a58a57859
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-08 09:33:03 -0300

    Changes at model

--- Backend/src/main/java/com/unep/wcmc/foodnutrition/repository/NutrientTypeRepository.java
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/repository/NutrientTypeRepository.java
@@ -1,17 +1,14 @@
 package com.unep.wcmc.foodnutrition.repository;
 
-import com.unep.wcmc.foodnutrition.model.MeasureUnit;
 import com.unep.wcmc.foodnutrition.model.NutrientType;
 import com.unep.wcmc.foodnutrition.support.AbstractRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.data.rest.core.config.Projection;
 
 import java.util.List;
-import java.util.Set;
 
-@RepositoryRestResource(path = "/nutrient-types", excerptProjection = NutrientTypeProjection.class)
+@RepositoryRestResource(path = "/nutrient-types")
 public interface NutrientTypeRepository extends AbstractRepository<NutrientType> {
 
     @RestResource(path = "nutrients")
@@ -19,18 +16,5 @@ public interface NutrientTypeRepository extends AbstractRepository<NutrientType>
 
     @RestResource(path = "name")
     NutrientType findByName(@Param("name") String name);
-
-}
-
-@Projection(name = "nutrientTypeProjection", types = NutrientType.class)
-interface NutrientTypeProjection {
-
-    Long getId();
-
-    String getName();
-
-    MeasureUnit getUnit();
-
-    Set<NutrientType> getChildren();
 
 }
