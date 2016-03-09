dce40ddda1e20978d8e6f06ab9811c20dfdc1a87
Rodrigo Silva <rcandidosilva@gmail.com>
2016-03-09 00:16:10 -0300

    Changes at nutrient's tab structure

--- Backend/src/main/java/com/unep/wcmc/foodnutrition/model/Nutrient.java
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/model/Nutrient.java
@@ -10,6 +10,7 @@ public class Nutrient implements BaseEntity, NutrientConstants {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private MeasureUnit unit;
 
     private Float value;
