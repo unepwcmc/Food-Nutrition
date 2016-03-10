feadc92234abb481477fe0a83e30967a02f8eebd
integritas-jozecarlos <i4525001314s>
2016-03-10 14:21:06 -0300

    refactoring taxon repository

--- Backend/src/main/java/com/unep/wcmc/foodnutrition/repository/TaxonRankRepository.java
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/repository/TaxonRankRepository.java
@@ -4,6 +4,6 @@ import com.unep.wcmc.foodnutrition.model.TaxonRank;
 import com.unep.wcmc.foodnutrition.support.AbstractRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "/taxon-rank")
+@RepositoryRestResource(path = "/taxon-ranks")
 public interface TaxonRankRepository extends AbstractRepository<TaxonRank> {
 }
