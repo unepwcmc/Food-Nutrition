21b1552ff411b4c2a776db98cf92547680d820ac
integritas-jozecarlos <i4525001314s>
2016-03-10 23:39:40 -0300

    Shape Form Model

---
+++ Backend/src/main/java/com/unep/wcmc/foodnutrition/model/ShapeForm.java
@@ -0,0 +1,34 @@
+package com.unep.wcmc.foodnutrition.model;
+
+import com.unep.wcmc.foodnutrition.support.BaseEntity;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class ShapeForm implements BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Override
+    public Long getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
