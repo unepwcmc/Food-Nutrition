62625442846978f02e6c61fbe352db587fe8803f
integritas-jozecarlos <i4525001314s>
2016-03-12 22:30:46 -0300

    food name tab, saving mode

--- Frontend/app/scripts/food/directives/food.name.description.directive.js
+++ Frontend/app/scripts/food/directives/food.name.description.directive.js
@@ -1,18 +1,61 @@
-define(['angularAMD'], function (angularAMD) {
+define(['angularAMD',
+
+    'core/factory/partPlantAnimalFactory',
+    'core/factory/foodProcessingFactory',
+    'core/factory/preservationMethodFactory',
+    'core/factory/cookingDegreeFactory',
+    'core/factory/agriculturalConditionsFactory',
+    'core/factory/ripenessMaturityFactory',
+
+    ], function (angularAMD) {
 
     'use strict';
 
-    angularAMD.directive('foodNameDescription', ['$timeout', '$rootScope', 'toastr',
-        function ( $timeout, $rootScope, toastr ) {
+    angularAMD.directive('foodNameDescription',
+        [
+            '$timeout',
+            '$rootScope',
+            'PartPlantAnimal',
+            'FoodProcessing',
+            'PreservationMethod'
+            ,'CookingDegree',
+            'AgriculturalConditions',
+            'RipenessMaturity',
+
+            function (
+                $timeout,
+                $rootScope,
+                PartPlantAnimal,
+                FoodProcessing,
+                PreservationMethod,
+                CookingDegree,
+                AgriculturalConditions,
+                RipenessMaturity) {
 
             return {
 
                 restrict: 'EA',
                 templateUrl: 'views/food/food.name.description.tab.tpl.html',
 
-                controller: ['$scope', '$http', '$rootScope', '$state', '$q',
-                    function( $scope, $http, $rootScope, $state, $q ) {
+                controller: ['$scope', function( $scope ) {
 
+                    $scope.partPlantAnimal = new PartPlantAnimal();
+                    $scope.partPlantAnimal.list();
+
+                    $scope.foodProcessing = new FoodProcessing();
+                    $scope.foodProcessing.list();
+
+                    $scope.preservationMethod = new PreservationMethod();
+                    $scope.preservationMethod.list();
+
+                    $scope.cookingDegree = new CookingDegree();
+                    $scope.cookingDegree.list();
+
+                    $scope.agriculturalConditions = new AgriculturalConditions();
+                    $scope.agriculturalConditions.list();
+
+                    $scope.ripenessMaturity = new RipenessMaturity();
+                    $scope.ripenessMaturity.list();
 
                 }],
                 link: function (scope, element, attrs) {
